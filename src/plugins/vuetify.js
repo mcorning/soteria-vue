27c6a399beac5860ed6d5ea822d9d9f5454773dc
Michael Corning <michael@secours.io>
2020-07-07 11:09:35 -0700

    undid code to get to mdi icons. cut cdn to axios

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import colors from 'vuetify/lib/util/colors';
-import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 
-Vue.use(Vuetify, {
-  iconfont: 'mdi' // 'md' || 'mdi' || 'fa' || 'fa4'
-});
+Vue.use(Vuetify);
+
 export default new Vuetify({
   theme: {
     themes: {
